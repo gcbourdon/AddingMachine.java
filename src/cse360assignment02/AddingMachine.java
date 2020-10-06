688dd17d489c3aa9203f96b473798c81ffeea591
Griffin Bourdon <gcbourdon@gmail.com>
2020-10-06 10:17:12 -0700

    Initial Commit: initial version of AddingMachine Project

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,34 @@
+/* Name: Griffin Bourdon
+   ASU Email Address: gbourdon@asu.edu
+   Lecture Time: Th 9am-10:15am
+*/
+
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    public AddingMachine() {
+        total = 0; // not needed - included for clarity
+    }
+
+    public int getTotal() {
+        return 0;
+    }
+
+    public void add (int value) {
+
+    }
+
+    public void subtract(int value) {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public void clear() {
+
+    }
+}
